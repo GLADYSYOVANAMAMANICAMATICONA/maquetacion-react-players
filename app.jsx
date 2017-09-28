61750e5470c3b59a7c6f07c5bc65f282bd285c86
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-09-28 17:09:18 -0500

    maquetacion con react

---
+++ app.jsx
@@ -0,0 +1,38 @@
+let PLAYERS = [
+  {
+    name: "Jim Hoskins",
+    score: 31,
+    id: 1,
+  },
+   {
+    name: "Andree Hoskins",
+    score: 35,
+    id: 2,
+  },
+   {
+    name: "Alena Hoskins",
+    score: 42,
+    id: 3,
+  },
+];
+
+let Header = React.createClass({//React.createClass---palabra reservada
+  //para imprimir en la pagina utilizar el metodo RENDER
+  render : function(){
+    return(
+      
+    )
+  }
+
+})
+
+
+// const Application = ({title, players}) => {
+//    return (
+//       <Header players={players}/>
+//       <PlayerList players={players}/>
+//       <PlayerForm />      
+//    ) ;
+// }
+
+// ReactDOM.render(<Application title="Scoreboard" players = {PLAYERS}/>, document.getElementById('container'));
